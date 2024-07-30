4f265ef16f9316bfec5148ce1f3ac62aa7e85d63
Alex Saldaña <alexdsa981@gmail.com>
2024-07-30 09:40:18 -0500

    Crea repositorio

---
+++ src/main/java/com/mb/mysteryboxpe/MysteryboxpeApplication.java
@@ -0,0 +1,13 @@
+package com.mb.mysteryboxpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysteryboxpeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysteryboxpeApplication.class, args);
+	}
+
+}
